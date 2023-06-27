3d099ec0683179b5324c5ad93cee265fb9f3a400
Akansha Aggarwal <akanshaaggarwal9876@gmail.com>
2023-06-27 16:24:23 +0530

    Redux and Toolkit Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-
+import {BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import Login from './scenes/loginPage/Login';
+import Profile from './scenes/profilePage/Profile';
+import Home from './scenes/homePage/Home';
 
 function App() {
   return (
-    <div className='app'> app
+    <div className='app'> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/home" element={<Home/>} />
+          <Route path='/profile/:userId' element={<Profile/>} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
